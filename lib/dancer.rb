1ee71f65a6629878199008a831094a8956690afe
Michael Bade <mbade314@gmail.com>
2020-05-22 15:33:44 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -7,3 +7,15 @@ class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
+class Dancer
+  extend MetaDancing
+end
+class Dancer
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
